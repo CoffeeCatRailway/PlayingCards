718398c24dc712c37d0e2fdda91a90e4d547a2ef
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-09-10 11:12:08 +1000

    Updated version

--- src/main/java/coffeecatteam/playingcards/Reference.java
+++ src/main/java/coffeecatteam/playingcards/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "playingcards";
     public static final String NAME = "Playing Cards";
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.1";
 
     public static final String DEPENDCIES = "after:vehicle@[0.23.0,);";
 
